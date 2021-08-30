6d833687fd0c4c2fc6099109a0aa58e59c2b4ccb
Azriel <aoirres@gmail.com>
2021-08-30 11:36:57 +0800

    added config php file - database configuration

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "62699";
+$dbname = "login_system_php";
+
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+if($conn->connect_error) {
+    die("Server error!" . mysqli::$connect_error);
+}
