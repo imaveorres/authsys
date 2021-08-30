2b8693affcac38ed4da9764c9fbd25459b439fe2
Azriel <aoirres@gmail.com>
2021-08-30 23:43:51 +0800

    modified state

--- src/js/jquery.js
+++ src/js/jquery.js
@@ -1,76 +1,80 @@
 
 // hide & show
 $(document).ready(function() {
-    $("#register-btn").click(function() {
-        $("#register-box").show();
-        $("#login-box").hide();
+    $('#register-btn').click(function() {
+        $('#register-box').show();
+        $('#login-box').hide();
     });
-    $("#login-btn").click(function() {
-        $("#register-box").hide();
-        $("#login-box").show();
+    $('#login-btn').click(function() {
+        $('#register-box').hide();
+        $('#login-box').show();
     });
-    $("#forgot-btn").click(function() {
-        $("#forgot-box").show();
-        $("#login-box").hide();
+    $('#forgot-btn').click(function() {
+        $('#forgot-box').show();
+        $('#login-box').hide();
     });
-    $("#back-btn").click(function() {
-        $("#forgot-box").hide();
-        $("#login-box").show();
+    $('#back-btn').click(function() {
+        $('#forgot-box').hide();
+        $('#login-box').show();
     });
 
     // form validate
-    $("#login-frm").validate();
-    $("#register-frm").validate({
+    $('#login-frm').validate();
+    $('#register-frm').validate({
         rules: {
             cpass: {
-                equalTo: "#pass"
+                equalTo: '#pass'
             }
         }
     });
 
-    $("#forgot-frm").validate();
+    $('#forgot-frm').validate();
 
     // submit form asynchronous
-    $("#register").click(function(e) {
-        if(document.querySelector("#register-frm").checkValidity()) {
+    $('#register').click(function(e) {
+        if(document.querySelector('#register-frm').checkValidity()) {
             e.preventDefault();
             $.ajax({
-                url: "action.php",
-                method: "POST",
-                data: $("#register-frm").serialize()+"&action=register",
+                url: 'action.php',
+                method: 'post',
+                data: $('#register-frm').serialize()+'&action=register',
                 success: function(res) {
-                    $("#alert").show();
-                    $("#result").html(res);
+                    $('#alert').show();
+                    $('#result').html(res);
                 }
             });
             return true;
         }
     });
-    $("#login").click(function(e) {
-        if(document.querySelector("#login-frm").checkValidity()) {
+    $('#login').click(function(e) {
+        if(document.querySelector('#login-frm').checkValidity()) {
             e.preventDefault();
             $.ajax({
-                url: "action.php",
-                method: "POST",
-                data: $("#login-frm").serialize()+"&action=login",
+                url: 'action.php',
+                method: 'post',
+                data: $('#login-frm').serialize()+'&action=login',
                 success: function(res) {
-                    $("#alert").show();
-                    $("#result").html(res);
+                    if(res === 'Session successfully set for user.') {
+                        window.location = 'profile.php';
+                    }else{
+                        $('#alert').show();
+                        $('#result').html(res);
+                    }
                 }
             });
             return true;
         }
     });
-    $("#forgot").click(function(e) {
-        if(document.querySelector("#forgot-frm").checkValidity()) {
+    $('#forgot').click(function(e) {
+        if(document.querySelector('#forgot-frm').checkValidity()) {
             e.preventDefault();
             $.ajax({
-                url: "action.php",
-                method: "POST",
-                data: $("#forgot-frm").serialize()+"&action=forgot",
+                url: 'action.php',
+                method: 'post',
+                data: $('#forgot-frm').serialize()+'&action=forgot',
                 success: function(res) {
-                    $("#alert").show();
-                    $("#result").html(res);
+                    $('#alert').show();
+                    $('#result').html(res);
                 }
             });
             return true;
