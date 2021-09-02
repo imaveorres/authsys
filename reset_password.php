2a2ea72c95ad5fcc9ab077fb6e992fe98498a5b1
Azriel <aoirres@gmail.com>
2021-09-02 22:22:32 +0800

    modified state

--- reset_password.php
+++ reset_password.php
@@ -1,38 +1,4 @@
-<?php
-require 'config.php';
-$msg = '';
-if(isset($_GET['email']) && isset($_GET['token'])) {
-    $email = $_GET['email'];
-    $token = $_GET['token'];
-    $stmt_sql_select_query = $conn->prepare("SELECT * FROM users WHERE email=? AND token=? AND token<>'' AND token_expire > NOW()");
-    $stmt_sql_select_query->bind_param('ss', $email,$token);
-    $stmt_sql_select_query->execute();
-    $result = $stmt_sql_select_query->get_result();
-    if($result->num_rows > 0) {
-        $stmt_sql_select_query->close();
-        if(isset($_POST['reset'])){
-            $newpass = sha1($_POST['newpass']);
-            $cnewpass = sha1($_POST['cnewpass']);
-            if($newpass == $cnewpass){
-                $stmt_sql_update_query = $conn->prepare("UPDATE users SET pass=?, token='' WHERE email=?");
-                $stmt_sql_update_query->bind_param('ss', $newpass,$email);
-                $stmt_sql_update_query->execute();
-                $msg = "Password changed successfully!<br><a href='index.php'>Login here.</a>";
-            }else{
-                echo 'Password did not match!';
-                exit();
-            }
-            $stmt_sql_update_query->close();
-        }
-    }else{
-        header('location:index.php');
-        exit();
-    }
-}else{
-    header('location:index.php');
-    exit();
-}
-?>
+<?php require 'reset_password_conf.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -41,23 +7,29 @@ if(isset($_GET['email']) && isset($_GET['token'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="src/css/main.css">
 </head>
 <body>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-lg-5 mt-5">
-                <h4 class="text-center lead"><?= $msg; ?></h3>
+                <div id="mssg-success">
+                    <h4 class="text-center lead" id="mssg-success-reset"><?= $msg; ?></h4>
+                </div>
+                <div class="text-center preloader">
+                    <img src="src/image/preloader.gif" alt="" class="m-2" id="loader">
+                </div>
                 <h5 class="text-center display-6 text-dark p-2 rounded">Reset your password here!</h3>
                 <form action="" id="resetpassword-frm" method="post">
-                    <div class="form-group">
+                    <div class="form-group mb-2">
                         <label for="password" class="form-label">Enter New Password</label>
-                        <input type="password" name="newpass" id="newpass" class="form-control" minlength="6" placeholder="New Password" required>
+                        <input type="password" name="newpass" id="newpass" class="form-control" minlength="6" placeholder="New password" required>
                     </div>
-                    <div class="form-group pt-3">
+                    <div class="form-group mb-2">
                         <label for="password" class="form-label">Confirm New Password</label>
-                        <input type="password" name="cnewpass" id="cnewpass" class="form-control" minlength="6" placeholder="Confirm New Password" required>
+                        <input type="password" name="cnewpass" id="cnewpass" class="form-control" minlength="6" placeholder="Confirm new password" required>
                     </div>
-                    <div class="form-group pt-3">
+                    <div class="form-group">
                         <input type="submit" name="reset" id="reset" value="Reset Password" class="btn btn-success btn-block form-control">
                     </div>
                 </form>
